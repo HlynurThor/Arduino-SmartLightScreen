204f6ba7512af5f80676b165835b7b747cbd887e
Hlynurxx <hlynurjxo@gmail.com>
2018-05-12 22:55:06 +0000

    update

--- Arduino_Dimmer/Arduino_controller/Arduino_controller.ino
+++ Arduino_Dimmer/Arduino_controller/Arduino_controller.ino
@@ -1,18 +1,34 @@
 int lights[8]{0,0,0,0,0,0,0,0};
+int lightsID[8]{8,7,6,5,4,0,0,0};
+int lightsIndicatorID[8]{8,7,6,5,4,0,0,0};
 
 void setup() {
   // put your setup code here, to run once:
- Serial.begin(9600);
-  Serial.println();
+  Serial.begin(9600);
+  Serial.println("Start");
+
+  pinMode(lightsID[0], OUTPUT);
+  pinMode(lightsID[1], OUTPUT);
+  pinMode(lightsID[2], OUTPUT);
+  pinMode(lightsID[3], OUTPUT);
+  pinMode(lightsID[4], OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 
-  fadeLight(0,10,96);
-delay(1000);
-  fadeLight(1,90,22);
+  fadeLight(0,90,0);
+ 
+  delay(1000);
+  fadeLight(1,0,90);
+  delay(1000);
+  fadeLight(2,90,5);
+  delay(1000);
+  fadeLight(3,0,20);
+  delay(1000);
+  fadeLight(4,90,2);
 
+    // watch from wifi if there is change. 
 
   delay(10000);
 }
@@ -48,8 +64,23 @@ void changeLightStatus(int LightID,int value){
 
     Serial.print("LightID: ");
     Serial.print(LightID); 
+    Serial.print(" Light pin: ");
+    Serial.print(lightsID[LightID]); 
     Serial.print(" value: ");
     Serial.print(value); 
-    Serial.println();
+    
+
+    if(value > 5)
+    {
+      digitalWrite(lightsID[LightID], HIGH);
+      Serial.print(" ON");
+    }
+    if(value <= 5)
+    {
+      digitalWrite(lightsID[LightID], LOW);
+      Serial.print(" OFF");
+    }
+
+    Serial.println();  
 }
 
